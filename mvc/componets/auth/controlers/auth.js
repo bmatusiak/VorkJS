81282ac0c3f695c238f0c43feaec193ce900598d
Bradley Matusiak <usabrad86@bellsouth.net>
2012-08-29 19:24:02 -0400

    update

--- mvc/componets/auth/controlers/auth.js
+++ mvc/componets/auth/controlers/auth.js
@@ -1,6 +1,15 @@
 //controler
 module.exports = function(vork) {
 
+    var callbackURL = getHost();
+    
+    function getHost(){
+       if(vork.req.headers.host.indexOf("rhcloud.com") >= 1){
+            return "vorkjs.bmatusiak.c9.io";
+       }else{
+           return vork.req.headers.host;
+       }
+    }
     var controler = {};
 
     //vork.layout = null;
@@ -12,22 +21,22 @@ module.exports = function(vork) {
     };
     controler.logout = function(callback){
           var user = vork.load.helper("user");
-          user.logout("http://vorkjs.bmatusiak.c9.io/",function(error){
+          user.logout(callbackURL+"/",function(error){
                 callback(null,{});
           });
     };
 
     controler.login = function(callback) {
-        var loginUrl = fb.authUrl("http://vorkjs.bmatusiak.c9.io/auth/callback", "email", callback);
+        var loginUrl = fb.authUrl(callbackURL+"/auth/callback", "email", callback);
         vork.res.redirect(loginUrl);
     };
     controler.callback = function(cb) {
         //fb.getAccessToken("http://" + vork.req.headers.host + "/auth/fb", function() {
-        fb.getAccessToken("http://vorkjs.bmatusiak.c9.io/auth/callback", function() {
+        fb.getAccessToken(callbackURL+"/auth/callback", function() {
             var user = vork.load.helper("user");
             user.checkLogin(1, null, function(err, obj) {
                 //vork.res.redirect("http://" + vork.req.headers.host + "/");
-                vork.res.redirect("http://vorkjs.bmatusiak.c9.io/");
+                vork.res.redirect(callbackURL+"/");
                 cb();
             })
         });
