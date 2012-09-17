1d087b12dd97da170cc2380ed46180c2680140e7
Bradley Matusiak <usabrad86@bellsouth.net>
2012-09-17 14:20:31 -0400

    update mardown to new parser

--- mvc/componets/md-wiki/init.js
+++ mvc/componets/md-wiki/init.js
@@ -4,12 +4,39 @@ module.exports = function(globals){
     init.ready = false;
     
     init.basepath = __dirname;
-    
-    //init.webroot = init.basepath+'/webroot'
-    
+     
     
     globals.md = require("node-markdown").Markdown;
     
+    try{
+        
+        globals.md2 = function(markdown){
+            var jsdom = require('jsdom').jsdom;
+            var html = require("./showdown.js").Showdown.converter().makeHtml(markdown);
+            var doc   = jsdom(html)
+            
+            for(var i in doc._ids){
+                var elements = doc._ids[i];
+                for(var j in elements){
+                    var element = elements[j];
+                    if(element.attributes.markdown){
+                        element.innerHTML = require("./showdown.js").Showdown.converter().makeHtml(element.innerHTML)
+                        //console.log(element.innerHTML);
+                        //console.log("--------")
+                    }
+                    /*for(var k in elementAttributes){
+                        var attribute = elementAttributes[k];
+                        //if(elementAttributes[k].id._tagName == "markdown"){
+                               console.log(k)
+                        //}
+                    }*/
+                }
+            }
+            return doc.innerHTML;
+        }
+    }catch(e){
+        console.log(e);
+    }
     
     return init;
 }
