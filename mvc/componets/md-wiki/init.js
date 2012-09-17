9a90a206a1c7e6f8ed0e92d428f2f4feac62be9e
Bradley Matusiak <usabrad86@bellsouth.net>
2012-09-17 14:29:27 -0400

    fixed for new markdown

--- mvc/componets/md-wiki/init.js
+++ mvc/componets/md-wiki/init.js
@@ -18,15 +18,7 @@ module.exports = function(globals){
                     var element = elements[j];
                     if(element.attributes.markdown){
                         element.innerHTML = require("./showdown.js").Showdown.converter().makeHtml(element.innerHTML)
-                        //console.log(element.innerHTML);
-                        //console.log("--------")
                     }
-                    /*for(var k in elementAttributes){
-                        var attribute = elementAttributes[k];
-                        //if(elementAttributes[k].id._tagName == "markdown"){
-                               console.log(k)
-                        //}
-                    }*/
                 }
             }
             return doc.innerHTML;
